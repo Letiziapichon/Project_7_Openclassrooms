5985607aca837ac03a42cfb44573866564d4a6ff
Letizia Pichon <letiziapichon@gmail.com>
2022-07-23 11:17:07 +0200

    Working version

--- backend/api.py
+++ backend/api.py
@@ -22,24 +22,13 @@ async def get_raw_data(selected_id: int):
     data_client = clients[clients.SK_ID_CURR == selected_id]
     return Response(data_client.to_json(orient="records"), media_type="application/json")
 
-# The ram available on the free version of heroku does not allow to return 
-# the output of the prediction in a time lower than the timeout
-# So we will use a file where the predictions have already been made
-# This code can however be run locally. 
 
 @app.post('/scoring/')
 async def get_scoring(selected_id: int):
     data = data_model[data_model.SK_ID_CURR == selected_id]
     data.drop(columns=['SK_ID_CURR'], inplace=True)
     score = model.predict_proba(data)
-    print(score)
     return score[0][0]
-""" 
-
-@app.get('/scoring/')
-async def get_scoring(selected_id: int):
-    score = model_score[model_score.SK_ID_CURR == selected_id]['score'].values[0]
-    return score"""
 
 @app.get('/fi_model/')
 async def get_fi_model():
